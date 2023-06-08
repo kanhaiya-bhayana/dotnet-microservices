5205aba84a5c469d1e36b1aa073280bb5a2b3c29
kanhaiya-bhayana <kbhayana60_be20@thapar.edu>
2023-06-08 18:30:00 +0530

    Feat: Frontend initiated and on-going CouponAPI consumption :construction:

---
+++ Mango/Mango.Web/Utility/StaticDetails.cs
@@ -0,0 +1,13 @@
+﻿namespace Mango.Web.Utility
+{
+    public class StaticDetails
+    {
+        public enum ApiType
+        {
+            GET, 
+            POST, 
+            PUT, 
+            DELETE
+        }
+    }
+}
