e3405e1a2863948b7019dd91a09992223df19031
kanhaiya-bhayana <kbhayana60_be20@thapar.edu>
2023-06-11 11:27:17 +0530

    Feat: Register endpoint integrated in AuthAPI microservice :heavy_check_mark:

---
+++ Mango/Mango.Services.AuthAPI/Models/Dto/RegistrationRequestDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mango.Services.AuthAPI.Models.Dto
+{
+    public class RegistrationRequestDto
+    {
+        public string Name { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Password { get; set; }
+    }
+}
