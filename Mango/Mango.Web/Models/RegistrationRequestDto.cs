000761c5e0fe154f208fe65a8de1fa42efa3e4f3
kanhaiya-bhayana <kbhayana60_be20@thapar.edu>
2023-06-13 10:48:45 +0530

    Feat: AuthAPI frontend initiated :construction:

---
+++ Mango/Mango.Web/Models/RegistrationRequestDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mango.Web.Models
+{
+    public class RegistrationRequestDto
+    {
+        public string Name { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Password { get; set; }
+        public string? Role { get; set; }
+    }
+}
