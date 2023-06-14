e20eabb3320788d05f61ba0e8ec574a170f49702
kanhaiya-bhayana <kbhayana60_be20@thapar.edu>
2023-06-14 16:29:14 +0530

    Fix: HttpClient for ProductService :lock:

--- Mango/Mango.Web/Program.cs
+++ Mango/Mango.Web/Program.cs
@@ -10,6 +10,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddHttpClient();
 builder.Services.AddHttpClient<ICouponService, CouponService>();
+builder.Services.AddHttpClient<IProductService, ProductService>();
 builder.Services.AddHttpClient<IAuthService, AuthService>();
 StaticDetails.CouponAPIBase = builder.Configuration["ServiceUrls:CouponAPI"];
 StaticDetails.AuthAPIBase = builder.Configuration["ServiceUrls:AuthAPI"];
