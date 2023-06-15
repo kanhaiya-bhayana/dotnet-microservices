52cd29dcf49c7f4a1d60f4d948f54ae3b02aaa9e
kanhaiya-bhayana <kbhayana60_be20@thapar.edu>
2023-06-15 16:58:30 +0530

    Feat: 4th microservice CartAPI completed :green_heart: :heavy_check_mark:

--- Mango/Mango.Services.ShoppingCartAPI/Controllers/CartAPIController.cs
+++ Mango/Mango.Services.ShoppingCartAPI/Controllers/CartAPIController.cs
@@ -18,12 +18,14 @@ namespace Mango.Services.ShoppingCartAPI.Controllers
         private ResponseDto _response;
         private readonly AppDbContext _db;
         private readonly IProductService _productService;
+        private readonly ICouponService _couponService;
 
-        public CartAPIController(AppDbContext appDbContext,IMapper mapper,IProductService productService)
+        public CartAPIController(AppDbContext appDbContext,IMapper mapper,IProductService productService,ICouponService couponService)
         {
             _db = appDbContext;
             _mapper = mapper;
             _productService = productService;
+            _couponService = couponService;
             this._response = new ResponseDto();
         }
 
@@ -45,6 +47,17 @@ namespace Mango.Services.ShoppingCartAPI.Controllers
                 {
                     item.Product = productDtos.FirstOrDefault(u=>u.ProductId == item.ProductId);
                     cart.CartHeader.CartTotal += (item.Count * item.Product.Price);
+                }
+
+                // apply coupon if any
+                if (!string.IsNullOrEmpty(cart.CartHeader.CouponCode)) {
+                    CouponDto coupon = await _couponService.GetCoupon(cart.CartHeader.CouponCode);
+                    if (coupon != null && cart.CartHeader.CartTotal > coupon.MinAmount)
+                    {
+                        cart.CartHeader.CartTotal -= coupon.DiscountAmount;
+                        cart.CartHeader.Discount = coupon.DiscountAmount;
+                    }
+                    
                 }
                 _response.Result = cart;
             }catch (Exception ex)
