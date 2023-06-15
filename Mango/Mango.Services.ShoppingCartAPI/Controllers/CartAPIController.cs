4eaf37805bd9b299ca7aa2507ee5606df1a53960
kanhaiya-bhayana <kbhayana60_be20@thapar.edu>
2023-06-15 16:07:22 +0530

    Feat: Interservice api call between CartAPI and ProductAPI :heavy_check_mark:

--- Mango/Mango.Services.ShoppingCartAPI/Controllers/CartAPIController.cs
+++ Mango/Mango.Services.ShoppingCartAPI/Controllers/CartAPIController.cs
@@ -2,6 +2,7 @@
 using Mango.Services.ShoppingCartAPI.Data;
 using Mango.Services.ShoppingCartAPI.Models;
 using Mango.Services.ShoppingCartAPI.Models.Dto;
+using Mango.Services.ShoppingCartAPI.Service.IService;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -16,11 +17,13 @@ namespace Mango.Services.ShoppingCartAPI.Controllers
         private readonly IMapper _mapper;
         private ResponseDto _response;
         private readonly AppDbContext _db;
+        private readonly IProductService _productService;
 
-        public CartAPIController(AppDbContext appDbContext,IMapper mapper)
+        public CartAPIController(AppDbContext appDbContext,IMapper mapper,IProductService productService)
         {
             _db = appDbContext;
             _mapper = mapper;
+            _productService = productService;
             this._response = new ResponseDto();
         }
 
@@ -36,8 +39,11 @@ namespace Mango.Services.ShoppingCartAPI.Controllers
                 cart.CartDetails = _mapper.Map<IEnumerable<CartDetailsDto>>(_db.CartDetails
                     .Where(u=>u.CartHeaderId == cart.CartHeader.CartHeaderId));
 
+                IEnumerable<ProductDto> productDtos = await _productService.GetProducts();
+
                 foreach(var item in cart.CartDetails)
                 {
+                    item.Product = productDtos.FirstOrDefault(u=>u.ProductId == item.ProductId);
                     cart.CartHeader.CartTotal += (item.Count * item.Product.Price);
                 }
                 _response.Result = cart;
--- Mango/Mango.Services.ShoppingCartAPI/Program.cs
+++ Mango/Mango.Services.ShoppingCartAPI/Program.cs
@@ -2,6 +2,8 @@ using AutoMapper;
 using Mango.Services.ShoppingCartAPI;
 using Mango.Services.ShoppingCartAPI.Data;
 using Mango.Services.ShoppingCartAPI.Extensions;
+using Mango.Services.ShoppingCartAPI.Service;
+using Mango.Services.ShoppingCartAPI.Service.IService;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.Filters;
@@ -17,7 +19,11 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
 builder.Services.AddSingleton(mapper);
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddHttpClient("Product", u => u.BaseAddress =
+new Uri(builder.Configuration["ServiceUrls:ProductAPI"]));
+builder.Services.AddHttpClient("Coupon", u => u.BaseAddress =
+new Uri(builder.Configuration["ServiceUrls:CouponAPI"]));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
