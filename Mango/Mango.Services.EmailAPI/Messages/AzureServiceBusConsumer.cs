a35e4d0f332decdf17043e0b1e2f979994288046
kanhaiya-bhayana <kbhayana60_be20@thapar.edu>
2023-06-20 09:49:44 +0530

    Feat: EmailLog implemented : zap:

--- Mango/Mango.Services.EmailAPI/Messages/AzureServiceBusConsumer.cs
+++ Mango/Mango.Services.EmailAPI/Messages/AzureServiceBusConsumer.cs
@@ -1,5 +1,6 @@
 ﻿using Azure.Messaging.ServiceBus;
 using Mango.Services.EmailAPI.Models;
+using Mango.Services.EmailAPI.Services;
 using Newtonsoft.Json;
 using System.Text;
 using System.Text.Json.Serialization;
@@ -11,10 +12,11 @@ namespace Mango.Services.EmailAPI.Messages
         private readonly string serviceBusConnectionString;
         private readonly string emailCartQueue;
         private readonly IConfiguration _configuration;
+        private readonly EmailService _emailService;
 
         private ServiceBusProcessor _emailCartProcessor;
 
-        public AzureServiceBusConsumer(IConfiguration configuration)
+        public AzureServiceBusConsumer(IConfiguration configuration, EmailService emailService)
         {
             _configuration = configuration;
             serviceBusConnectionString = _configuration.GetValue<string>("ServiceBusConnectionString");
@@ -23,6 +25,7 @@ namespace Mango.Services.EmailAPI.Messages
 
             var client = new ServiceBusClient(serviceBusConnectionString);
             _emailCartProcessor = client.CreateProcessor(emailCartQueue);
+            _emailService = emailService;
             //_emailCartProcessor.
         }
 
@@ -48,6 +51,7 @@ namespace Mango.Services.EmailAPI.Messages
             try
             {
                 // TODO - try to log email
+                await _emailService.EmailCartAndLog(objMessage);
                 await args.CompleteMessageAsync(args.Message);
             }catch (Exception ex) { throw; }
         }
--- Mango/Mango.Services.EmailAPI/Program.cs
+++ Mango/Mango.Services.EmailAPI/Program.cs
@@ -1,6 +1,7 @@
 using Mango.Services.EmailAPI.Data;
 using Mango.Services.EmailAPI.Extension;
 using Mango.Services.EmailAPI.Messages;
+using Mango.Services.EmailAPI.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +11,13 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+var optionBuilder = new DbContextOptionsBuilder<AppDbContext>();
+optionBuilder.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+builder.Services.AddSingleton(new EmailService(optionBuilder.Options));
+
+// We can not consume a scoped service in a singleton implementation
+
 builder.Services.AddSingleton<IAzureServiceBusConsumer, AzureServiceBusConsumer>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
